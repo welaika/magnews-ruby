6ea3895909fa1326a924d0ed42b89281e71917f8
Fabrizio Monti <fabrizio.monti@welaika.com>
2015-07-06 16:04:20 +0200

    Add config for rspec status file

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,6 +23,8 @@ RSpec.configure do |config|
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
+
+  config.example_status_persistence_file_path = "./spec/examples.txt"
 end
 
 # WebMock.allow_net_connect!
